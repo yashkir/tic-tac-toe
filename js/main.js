6df8a30be0385b3f1d1273b6b16ba05f1f57eb0c
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-17 17:10:20 -0400

    implement MVP logic, game is functional

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@ class TicTacToe {
         [2, 4, 6],
     ];
 
+    static symbols = {
+        '-1' : 'X',
+        '1': 'O',
+        '0': '',
+    }
+
     constructor (selector) {
         this.squares = [
             0, 0, 0,
@@ -19,11 +25,15 @@ class TicTacToe {
             0, 0, 0,
         ];
 
-        this.activePlayer = -1;
+        this.turn = 0;
+        this.lastTurn = 8;
         this.games = [];
+
+        this.activePlayer = -1;
 
         this.boardEl = document.querySelector(`${selector} .board`);
         this.squareEls = document.querySelectorAll(`${selector} .board > .square`);
+        this.statusEl = document.querySelector(`${selector} .status`);
         this.resetGameBtn = document.querySelector(`${selector} .reset-game-btn`);
         
         this.boardEl.addEventListener("click", this.boardClickHandler.bind(this));
@@ -31,16 +41,65 @@ class TicTacToe {
     }
 
     renderSquare(n) {
-        console.log(n);
-        this.squareEls[n].innerHTML = this.squares[n];
+        this.squareEls[n].innerHTML = this.constructor.symbols[this.squares[n]];
+    }
+
+    renderWinner(winner) {
+        let text = "";
+
+        if (winner === 0) {
+            text = "It's a tie!";
+        } else {
+            text = `Winner: ${this.constructor.symbols[winner]}`;
+        }
+
+        this.statusEl.textContent = text;
     }
 
     boardClickHandler(event) {
-        console.log(event);
         let n = event.target.id[event.target.id.length - 1] - 1;
 
-        this.squares[n] = this.activePlayer;
-        this.renderSquare(n);
+        if (this.squares[n] === 0) {
+            this.squares[n] = this.activePlayer;
+
+            this.renderSquare(n);
+
+            // TODO move the logic out of the click handler
+            let winner = this.getWinner();
+            if (winner !== null) {
+                this.renderWinner(winner); 
+            }
+
+            this.activePlayer *= -1;
+            this.turn++;
+        }
+    }
+
+    /* return winning player index, or 0 for a tie, null otherwise */
+    getWinner() {
+        for (const line of this.constructor.winningLines) {
+            let lookingAt = 0;
+            let count = 1;
+
+            for (const index of line) {
+                if (this.squares[index] === lookingAt) {
+                    count++;
+                } else {
+                    lookingAt = this.squares[index];
+                    count = 1;
+                }
+            }
+
+            if (lookingAt != 0 && count === 3) {
+                return lookingAt;
+            }
+        }
+
+        if (this.turn === this.lastTurn) {
+            return 0;
+        } else {
+            return null;
+        }
     }
 
     resetGame() {
